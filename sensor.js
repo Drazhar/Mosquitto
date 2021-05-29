8ebfb170c70a93146bdc086bc5db81ef5509a613
Philip <philip.trauth@gmx.de>
2021-05-29 07:40:11 +0200

    Some refactoring

--- sensor.js
+++ sensor.js
@@ -4,25 +4,17 @@ const mqtt = require("mqtt")
 const client = mqtt.connect("mqtt://mosquitto")
 
 client.on("connect", function () {
-  client.subscribe("home/temperatures/boiler", function (err) {
-    if (!err) {
-      setInterval(function () {
-        client.publish("home/temperatures/boiler", `${sensor.readC("28-3c01f095f2f9")}`)
-      }, 60000)
-    }
-  })
-  client.subscribe("home/temperatures/garten", function (err) {
-    if (!err) {
-      setInterval(function () {
-        client.publish("home/temperatures/garten", `${sensor.readC("28-3c01f095ddc3")}`)
-      }, 60000)
-    }
-  })
-  client.subscribe("home/temperatures/keller", function (err) {
-    if (!err) {
-      setInterval(function () {
-        client.publish("home/temperatures/keller", `${sensor.readC("28-3c01f095ce5d")}`)
-      }, 60000)
-    }
-  })
+  subscribeSensor("home/temperatures/boiler", "28-3c01f095f2f9")
+  subscribeSensor("home/temperatures/garten", "28-3c01f095ddc3")
+  subscribeSensor("home/temperatures/keller", "28-3c01f095ce5d")
 })
+
+function subscribeSensor(mosquittoString, sensorString) {
+  client.subscribe(mosquittoString, function (err) {
+    if (!err) {
+      setInterval(function () {
+        client.publish(mosquittoString, `${sensor.readC(sensorString)}`)
+      }, 60000)
+    }
+  })
+}
